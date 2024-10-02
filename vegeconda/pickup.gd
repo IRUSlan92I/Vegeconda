1dfe06869dd33f0b442fbd122188698544d3b430
Ruslan Ignatov <ruslanlena30@gmail.com>
2024-10-02 18:20:33 +0300

    Added more left animations

--- vegeconda/pickup.gd
+++ vegeconda/pickup.gd
@@ -11,49 +11,49 @@ enum PickupState {PREPARING, SHOWING_UP, IDLING, HIGHLIGHTING}
 
 
 const ANIMATIONS_BY_TYPE := {
-    PickupType.CHERRY: "cherry",
-    PickupType.FLY_AGARIC: "fly_agaric",
-    PickupType.GREEN_APPLE: "green_apple",
-    PickupType.RED_APPLE: "red_apple",
+	PickupType.CHERRY: "cherry",
+	PickupType.FLY_AGARIC: "fly_agaric",
+	PickupType.GREEN_APPLE: "green_apple",
+	PickupType.RED_APPLE: "red_apple",
 }
 
 const ANIMATIONS_BY_STATE := {
-    PickupState.PREPARING: "preparing",
-    PickupState.SHOWING_UP: "showing_up",
-    PickupState.IDLING: "idling",
-    PickupState.HIGHLIGHTING: "highlighting",
+	PickupState.PREPARING: "preparing",
+	PickupState.SHOWING_UP: "showing_up",
+	PickupState.IDLING: "idling",
+	PickupState.HIGHLIGHTING: "highlighting",
 }
 
 
 func _ready() -> void:
-    _play_animation()
+	_play_animation()
 
 
 func _play_animation() -> void:
-    var animation : String
-    if state == PickupState.PREPARING:
-        animation = ANIMATIONS_BY_STATE[state]
-    else:
-        animation = "%s_%s" % [
-            ANIMATIONS_BY_TYPE[type], 
-            ANIMATIONS_BY_STATE[state]
-        ]
-    $AnimatedSprite2D.play(animation)
+	var animation : String
+	if state == PickupState.PREPARING:
+		animation = ANIMATIONS_BY_STATE[state]
+	else:
+		animation = "%s_%s" % [
+			ANIMATIONS_BY_TYPE[type], 
+			ANIMATIONS_BY_STATE[state]
+		]
+	$AnimatedSprite2D.play(animation)
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-    match state:
-        PickupState.PREPARING:
-            state = PickupState.SHOWING_UP
-            _play_animation()
-        PickupState.SHOWING_UP:
-            state = PickupState.IDLING
-            _play_animation()
-        PickupState.HIGHLIGHTING:
-            state = PickupState.IDLING
-            _play_animation()
+	match state:
+		PickupState.PREPARING:
+			state = PickupState.SHOWING_UP
+			_play_animation()
+		PickupState.SHOWING_UP:
+			state = PickupState.IDLING
+			_play_animation()
+		PickupState.HIGHLIGHTING:
+			state = PickupState.IDLING
+			_play_animation()
 
 
 func highlight() -> void:
-    state = PickupState.HIGHLIGHTING
-    _play_animation()
+	state = PickupState.HIGHLIGHTING
+	_play_animation()
--- vegeconda/snake_segment.gd
+++ vegeconda/snake_segment.gd
@@ -2,4 +2,4 @@ extends StaticBody2D
 
 
 func _ready() -> void:
-    pass
+	pass
